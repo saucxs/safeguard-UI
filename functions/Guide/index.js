0b82f321b8eae2c4c6cc5f2bd465b6e18ddaa8bb
saucxs <184866445@qq.com>
2017-03-20 16:57:25 +0800

    no message

--- functions/Guide/index.js
+++ functions/Guide/index.js
@@ -1,3 +1,15 @@
 /**
  * Created by BFD-638 on 2017/3/18.
  */
+import React from 'react'
+import Center from 'public/Center'
+import Markdown from 'public/Markdown'
+import html from '../../README.md'
+
+export default () => {
+  return (
+    <Center>
+      <Markdown html={html} />
+    </Center>
+  )
+}
